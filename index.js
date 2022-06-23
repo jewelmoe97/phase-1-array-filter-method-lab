ef9328edc990e7affabbf12aa97945a148f23f67
jewel <snowdecember079@gmail.com>
2022-06-22 23:24:51 -0400

    commit tree e70bb28d5c9787d5a09e39947fafe09b6f4c76a4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers,names) {
+    return drivers.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === names.toLowerCase()
+      );
+    }
+function fuzzyMatch(drivers,letters) {
+    return drivers.filter(
+        (possibleMatch) =>
+          possibleMatch.toLowerCase().indexOf(letters.toLowerCase()) === 0
+      );
+    
+}
+function matchName(drivers,driverNames) {
+    return drivers.filter((record) => record.name === driverNames);
+}
